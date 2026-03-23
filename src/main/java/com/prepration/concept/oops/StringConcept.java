26343576c6ca2486f6ecfe9f21cea1e39ed851ff
shushant4120 <shushant.sjb96@gmail.com>
2026-03-23 23:17:50 +0530

    String playground

--- src/main/java/com/prepration/concept/oops/StringConcept.java
+++ src/main/java/com/prepration/concept/oops/StringConcept.java
@@ -1,13 +1,44 @@
 package com.prepration.concept.oops;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class StringConcept {
-    
-    String s1 = "sky";
-    String s2= "nipu";
 
     public static void main(String[] args) {
-        StringConcept sc = new StringConcept();
-        sc.s1 = sc.s2;
-        System.out.println(sc.s1);
+
+        String s1 = "Shushant";
+        String s2 = "Shushant";
+        String s3 = new String("Shushant");
+        // 👉 intern() → moves reference to String Pool
+        // System.out.println(s1==s3.intern());
+        // System.out.println(s1.hashCode() + " "+ s2.hashCode() + " "+ s3.hashCode());
+        // System.out.println(s1.concat(" Kumar") + " "+ s1);
+        String s4 = "";
+        Map<Character, Integer> map = new HashMap<>();
+        char[] ch = s1.toLowerCase().toCharArray();
+        for (char c : ch) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            s4 += c;
+        }
+
+        List<Map.Entry<Character, Integer>> list = map.entrySet().stream().filter(n -> n.getValue() > 1).toList();
+        list.forEach(n -> System.out.print(n.getKey() + " " + n.getValue()));
+
+        String s5 = "Sameer";
+        StringBuilder sb = new StringBuilder(s5);
+        sb.append("Kumar");
+        sb.reverse();
+        System.out.println(sb);
+
+        String s6 = "";
+        System.out.println(s5.length());
+        for(int i=s5.length()-1; i>=0;i--){
+
+           s6+= s5.charAt(i);
+        }
+        System.out.println(s6);
     }
 }
