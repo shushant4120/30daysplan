7ebd6c80e07e64ce927f39241d11b986f6b6ce80
shushant4120 <shushant.sjb96@gmail.com>
2026-03-31 12:42:28 +0530

    implemetation

--- src/main/java/com/prepration/concept/designpattern/factorypattern/CallVehicle.java
+++ src/main/java/com/prepration/concept/designpattern/factorypattern/CallVehicle.java
@@ -5,5 +5,6 @@ public class CallVehicle {
     public static void main(String[] args) {
         Vehicle car= VehicleFactory.getVehicle("car");
         car.drive();
+        VehicleFactory.getVehicle("bike").drive();
     }
 }
