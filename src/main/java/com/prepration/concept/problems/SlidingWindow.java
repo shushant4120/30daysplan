71affdaf00e64706f3bdc737e40290a8c6856a04
shushant4120 <shushant.sjb96@gmail.com>
2026-04-04 22:32:12 +0530

    new method

--- src/main/java/com/prepration/concept/problems/SlidingWindow.java
+++ src/main/java/com/prepration/concept/problems/SlidingWindow.java
@@ -19,6 +19,25 @@ public class SlidingWindow {
 
         return sum;
     }
+    public int maxSumSubarray(int[] arr, int k) {
+    int sum = 0;
+
+    // Step 1: first window
+    for (int i = 0; i < k; i++) {
+        sum += arr[i];
+    }
+
+    int max = sum;
+
+    // Step 2: slide window
+    for (int i = k; i < arr.length; i++) {
+        sum = sum - arr[i - k]; // remove left
+        sum = sum + arr[i];     // add right
+        max = Math.max(max, sum);
+    }
+
+    return max;
+}
     public static void main(String[] args) {
         int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
         int k = 3;
