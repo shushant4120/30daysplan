635ead859770d4863ae407ad343e48bac6687f18
shushant4120 <shushant.sjb96@gmail.com>
2026-04-10 22:45:23 +0530

    global exception

--- src/main/java/com/prepration/concept/GlobalExceptionHandler.java
+++ src/main/java/com/prepration/concept/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.prepration.concept;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,4 +12,12 @@ public class GlobalExceptionHandler {
         System.out.println("Global exception handler " + e);
         return "error";
     }
+    public void methodA() throws IOException {
+    methodB();
+}
+
+public void methodB() throws IOException {
+    throw new IOException("File error");
+}
+
 }
