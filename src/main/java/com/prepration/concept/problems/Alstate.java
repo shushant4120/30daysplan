84343c32657deb9578aa38da461208f677d2c8a8
shushant4120 <shushant.sjb96@gmail.com>
2026-04-12 11:20:32 +0530

    hasmap and linked hash map question

---
+++ src/main/java/com/prepration/concept/problems/Alstate.java
@@ -0,0 +1,32 @@
+package com.prepration.concept.problems;
+
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Alstate {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name= "Shushant";
+       // s2h2u1a1n1t1
+
+       char[] charArray = name.toLowerCase().toCharArray();
+       System.out.println(name.length());
+       //LinkedHashMap maintain insertion order and HashMap does not maintain any order
+       Map<Character, Integer> charCountMap = new LinkedHashMap<>();
+       Map<Character, Integer> charCountMap1 = new HashMap<>();
+
+       for(int i=name.length()-1; i>=0;i--){
+       
+        charCountMap.put(charArray[i], charCountMap.getOrDefault(charArray[i], 0)+1);
+        charCountMap1.put(charArray[i], charCountMap1.getOrDefault(charArray[i], 0)+1);
+
+
+       }
+       System.out.println(charCountMap);
+       System.out.println(charCountMap1);
+
+    }
+}
